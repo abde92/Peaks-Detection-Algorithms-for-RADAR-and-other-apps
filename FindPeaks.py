e98ee97965f7f24088e41837ad4d03c849a8c84c
UserBelissaoui <abd.belissaoui@gmail.com>
2021-05-31 17:18:42 +0100

    Adding Multiple Peaks Finding based Basline(average, median)  Algorithms

--- FindPeaks.py
+++ FindPeaks.py
@@ -2,6 +2,7 @@
 # Details of these algorithms can be found in this link: https://www.baeldung.com/cs/signal-peak-detection
 import numpy as np
 import scipy as sp
+import statistics
 
 #1. Algorithm : Find spike (Naive Peak Finding)
  # Input : Measured Data
@@ -21,15 +22,54 @@ def FindSpike(data):
 
 
 
-#2. Algorithm : Find multiple peaks (Baseline =  Average)
+#2. Algorithm : Multiple peaks finding (Baseline =  Average)
  # Input : Measured Data
- # Output : Peaks indexe
+ # Output : Peaks indices
+
+def FindPeaks_Basline_Av(data):
+    peaksIndexs = []
+    peak_id = 0
+    peak_V = 0
+    baseline = statistics.mean(data)
+
+    for id in range(len(data)):
+        if  data[id] > baseline: 
+            if peak_V == 0 or data[id] > peak_V:
+                peak_id = id
+                peak_V = data[id]
+
+        elif data[id] < baseline and peak_id != 0:
+            peaksIndexs.append(peak_id)        
+            peak_id = 0
+            peak_V = 0
+    if peak_id != 0: 
+        peaksIndexs.append(peak_id)       
+
+    return peaksIndexs
+
+#3. Algorithm : Multiple peaks finding (Baseline =  Median)
+ # Input : Measured Data
+ # Output : Peaks indices
 
 
-def FindPeaks_Basline():
-    peaksInedxs = []
-    peaksValues = []
+def FindPeaks_Basline_Md(data):
+    peaksIndices = []
+    peak_id = 0
+    peak_V = 0
+    baseline = statistics.median(data)
+
+    for id in range(len(data)):
+        if  data[id] > baseline: 
+            if peak_V == 0 or data[id] > peak_V:
+                peak_id = id
+                peak_V = data[id]
+
+        elif data[id] < baseline and peak_id != 0:
+            peaksIndices.append(peak_id)        
+            peak_id = 0
+            peak_V = 0
+    if peak_id != 0: 
+        peaksIndices.append(peak_id)       
+
+    return peaksIndices
     
-    
-    
-    return peaksInedxs, peaksValues
--- Run_Algorithms.py
+++ Run_Algorithms.py
@@ -1,5 +1,7 @@
-# Run algorithms to find peaks 
+""" In order to develop and expand my python programming skills, I decided to start 
+coding algrithms even in case they are already implemented by other python developers """
 
+# Run algorithms to find peaks 
 import numpy as np
 import scipy as sp
 import math as mth
@@ -16,19 +18,36 @@ s[100] = 3
 s[200] = 5
 s[505] = 6
 
-
-# find spike value and its index in data
-output = fp.FindSpike(s)
-
-print(output)
+# ----------------------Find Spike ---------------------#
+SpikeIndex,SpikeValue = fp.FindSpike(s)
 plt.plot(x_axis,s)
-#TODO : mark the spike on the plot 
-# plt.plot(x_axis,s[peakIndex])
+#TODO : mark the spike on the plot -Done
+plt.plot(x_axis[SpikeIndex],s[SpikeIndex],'rD')
+plt.xlabel('Time(ms)') 
+plt.ylabel('Amplitude(mV)') 
+plt.title("Naive Peak(spike) Finding Algorithm")
 plt.show()
 
+# ----- Multiple Peaks Finding - Baseline = Average-----#
+peakIndices_Av = fp.FindPeaks_Basline_Av(s)
+plt.plot(x_axis,s)
+#TODO : mark peaks on the plot - Done
+plt.plot(x_axis[peakIndices_Av],s[peakIndices_Av],'rD')
+plt.xlabel('Time(ms)') 
+plt.ylabel('Amplitude(mV)') 
+plt.title("Multpile Peaks Finding Based Average-Basline lgorithm ")
+plt.show()
 
+# ----- Multiple Peaks Finding - Baseline = Average-----#
 
-
+peakIndices_Md = fp.FindPeaks_Basline_Md(s)
+plt.plot(x_axis,s)
+#TODO : mark peaks on the plot - Done
+plt.plot(x_axis[peakIndices_Md],s[peakIndices_Md],'rD')
+plt.xlabel('Time(ms)') 
+plt.ylabel('Amplitude(mV)') 
+plt.title("Multpile Peaks Finding Based Median-Basline lgorithm ")
+plt.show()
 
 
 
