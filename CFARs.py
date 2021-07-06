d7a921d18f7bf58d7dfeb9494798dd452eb2ed2b
UserBelissaoui <abd.belissaoui@gmail.com>
2021-07-06 11:27:32 +0100

    Cod cleaning and plots enhancing

--- CFARs.py
+++ CFARs.py
@@ -1,3 +1,24 @@
-def ca_cfar():
 
-      return indices
+import numpy as np
+
+def ca_cfar(data_matrix, refLength, guardLength, rate_fa):
+    alpha = refLength*(rate_fa**(-1/refLength) - 1) # threshold factor calculated done (y)
+    
+    #TODO square-law detector before performing cfar algorithm !
+
+    cfarWin= np.ones(((refLength+guardLength)*2+1,1))
+    cfarWin[refLength+1:refLength+1+2*guardLength]=0
+    cfarWin=cfarWin/np.sum(cfarWin)
+
+    #np.reshape(data_matrix, data_matrix.size)
+    #np.reshape(cfarWin, cfarWin.size)
+
+    noiseLevel= np.convolve(data_matrix.squeeze(), cfarWin.squeeze(), 'same')
+    ca_cfarThreshold = noiseLevel+ alpha
+
+    
+    return ca_cfarThreshold
+
+def os_cfar (data_matrix, os_cfar_parms):
+
+    return os_cfarThreshold
--- Run_Algorithms.py
+++ Run_Algorithms.py
@@ -1,7 +1,7 @@
 # By Abderrahim BELISSAOUI
 # If you are student, feel free to use these codes
 # If you are professional, let me know , send message here abd.belissaoui@gmail
-# Run algorithms to find peaks 
+
 import numpy as np
 import scipy as sp
 import pandas as pd
@@ -9,6 +9,7 @@ import math as mth
 from scipy.fftpack import fft, fftfreq, fftshift
 import matplotlib.pyplot as plt
 import FindPeaks as fp
+import CFARs 
 
 # ------------------------------------------ Synthetic Data ---------------------------------#
 #### Synthetic data  : sum of sine waves
@@ -16,41 +17,48 @@ import FindPeaks as fp
 def SineWAve(SampleRate,StartTime, EndTime,frq1,frq2,frq3):
     
     StepTime = 50/SampleRate
-    t_axis = np.arange(StartTime,EndTime, StepTime)
-    s = 0.4*np.sin(2*mth.pi*frq1*t_axis) + 1.5*np.cos(2*mth.pi*frq2*t_axis) +0.9*np.sin(2*mth.pi*frq3*t_axis)
+    f_axis = np.arange(StartTime,EndTime, StepTime)
+    s = 0.4*np.sin(2*mth.pi*frq1*f_axis) + 1.5*np.cos(2*mth.pi*frq2*f_axis) +0.9*np.sin(2*mth.pi*frq3*f_axis)
     
-    return t_axis, s
+    return f_axis, s
 
 
-t_axis , s = SineWAve(SampleRate = 500,StartTime = 0, EndTime = 10 ,frq1 = 20, frq2 = 80, frq3 = 150)
+f_axis , s = SineWAve(SampleRate = 500,StartTime = 0, EndTime = 10 ,frq1 = 20, frq2 = 80, frq3 = 150)
 
-# Add random signal ( Noise)
+# Add  Noise ( a random data) and generate the synthetic signal
 L = len(s)
 s  = s + np.random.randn(L)
 
 # ------------------------------------------ FMCW RADAR raw data ---------------------------------#
-# Read csv data
-# Download RADAR I/Q data : https://drive.google.com/file/d/1FiApaaWw0SeDA2giC1a6G4EUcVcLrwZ6/view?usp=sharing
+# Download FMCW RADAR I/Q data : https://drive.google.com/file/d/1FiApaaWw0SeDA2giC1a6G4EUcVcLrwZ6/view?usp=sharing
 
-IQ_data = pd.read_csv('/home/issa/My Files/SelfLearning/Github/Datasets/IQdata.csv', usecols = ["I", "Q"] )
+# Read csv data
+IQ_data = pd.read_csv('your data path here/IQdata.csv', usecols = ["I", "Q"] )
 IQ_raw_data = IQ_data["I"] + 1j*IQ_data["Q"]
 N = len(IQ_raw_data)
 Q_raw_data_F = fft(np.array(IQ_raw_data))
 freq = fftfreq(N, 1/(5*10**5))[:N//2]
-# IQ_FFT = fftshift(fft(np.array(IQ_raw_data), n=256, axis=- 1))
-# s = (np.abs(IQ_FFT))/np.max(np.abs(IQ_FFT))
-# freq = fftshift(fftfreq(n=256))
 s = 2.0/N * np.abs(Q_raw_data_F[0:N//2])
-t_axis = freq 
+f_axis = freq 
+
 #TODO: Define frequencies axis then range axis;  Status: Done
+
+# CA_CFAR parameters
+#TODO : organise CFARs parameters in 
+CACFAR_ref_len = 5
+CACFAR_grd_len = 2
+fa = 0.001
 
 # ----------------------Find Spike ---------------------#
 SpikeIndex,SpikeValue = fp.FindSpike(s)
+cacfar_original_fft_mag = CFARs.ca_cfar(s,CACFAR_ref_len,CACFAR_grd_len,fa) 
+
 plt.figure(1)
-plt.plot(t_axis,s)
+plt.plot(f_axis,s)
+plt.plot(f_axis,cacfar_original_fft_mag,'k')
 #TODO : mark the spike on the plot -Done
-plt.plot(t_axis[SpikeIndex],s[SpikeIndex],'rD')
-plt.xlabel('Time(ms)') 
+plt.plot(f_axis[SpikeIndex],s[SpikeIndex],'rD')
+plt.xlabel('Frequency(Hz)') 
 plt.ylabel('Amplitude(mV)') 
 plt.title("Naive Peak(spike) Finding Algorithm")
 plt.show()
@@ -58,39 +66,41 @@ plt.show()
 # ----- Multiple Peaks Finding - Baseline = Average-----#
 peakIndices_Av = fp.FindPeaks_Basline_Av(s)
 plt.figure(2)
-plt.plot(t_axis,s)
+plt.plot(f_axis,s)
 #TODO : mark peaks on the plot - Done
-plt.plot(t_axis[peakIndices_Av],s[peakIndices_Av],'rD')
-plt.xlabel('Time(ms)') 
+plt.plot(f_axis[peakIndices_Av],s[peakIndices_Av],'rD')
+plt.xlabel('Frequency(Hz)') 
 plt.ylabel('Amplitude(mV)') 
 plt.title("Multpile Peaks Finding Based Average-Basline lgorithm ")
 plt.show()
 
 # ----- Multiple Peaks Finding - Baseline = Median-----#
-
 peakIndices_Md = fp.FindPeaks_Basline_Md(s)
 plt.figure(3)
-plt.plot(t_axis,s)
+plt.plot(f_axis,s)
 #TODO : mark peaks on the plot - Done
-plt.plot(t_axis[peakIndices_Md],s[peakIndices_Md],'rD')
-plt.xlabel('Time(ms)') 
+plt.plot(f_axis[peakIndices_Md],s[peakIndices_Md],'rD')
+plt.xlabel('Frequency(Hz)') 
 plt.ylabel('Amplitude(mV)') 
 plt.title("Multpile Peaks Finding Based Median-Basline lgorithm ")
 plt.show()
 
 # ----- Multiple Peaks Finding - Noisy Peaks - Baseline = Average-----#
-
 peakIndices_Smoth, smoothed_s = fp.FindPeaks_Basline_Smoth(s)
+cacfar_original_s = CFARs.ca_cfar(s,CACFAR_ref_len,CACFAR_grd_len,fa) 
+cacfar_smothed_s = CFARs.ca_cfar(smoothed_s,CACFAR_ref_len,CACFAR_grd_len,fa) 
 
 fig4 = plt.figure(4)
 plt.subplot(211)
-plt.plot(t_axis,s,'b--', label='Signal')
+plt.plot(f_axis,s,'b--', label='Signal')
+plt.plot(f_axis,cacfar_original_s,'k--', label='ca-cfar threshold')
 plt.legend()
 plt.subplot(212)
-plt.plot(t_axis,smoothed_s,color='k', label='Smoothed signal')
+plt.plot(f_axis,smoothed_s,color='k', label='Smoothed signal')
+plt.plot(f_axis,cacfar_smothed_s,color='r', label='ca-cfar threshold')
 # #TODO : mark peaks on the plot - Done
-plt.plot(t_axis[peakIndices_Smoth],smoothed_s[peakIndices_Smoth],'rD', label='Peaks')
-plt.xlabel('Time(ms)') 
+plt.plot(f_axis[peakIndices_Smoth],smoothed_s[peakIndices_Smoth],'rD', label='Peaks')
+plt.xlabel('Frequency(Hz)') 
 plt.ylabel('Amplitude(mV)') 
 fig4.suptitle("Multpile Peaks Finding Based  Smoothing and Average-Basline Algorithm ")
 plt.legend()
@@ -99,10 +109,10 @@ plt.show()
 # ----- Wide Peaks Finding - Baseline = Average-----#
 peakIndices_FWP = fp.FindWidePeaks(s)
 plt.figure(5)
-plt.plot(t_axis,s)
+plt.plot(f_axis,s)
 #TODO : mark peaks on the plot - Done
-plt.plot(t_axis[peakIndices_FWP],s[peakIndices_FWP])
-plt.xlabel('Time(ms)') 
+plt.plot(f_axis[peakIndices_FWP],s[peakIndices_FWP])
+plt.xlabel('Frequency(Hz)') 
 plt.ylabel('Amplitude(mV)') 
 plt.title("Wide Peaks Finding Algorithm ")
 plt.show()
